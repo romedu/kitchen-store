3f2cce76459523281c6b1b7d0690e7b6c499fb06
Roberto Meran <rmeran@contractor.appsbees.com>
2021-07-16 19:39:59 -0400

    feat: Implement error handling in the ProductModal component

--- pages/components/ProductModal.js
+++ pages/components/ProductModal.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Modal } from "@shopify/polaris";
 import ProductDetailsView from "./ProductDetailsView/ProductDetailsView";
+import ErrorModal from "./ErrorModal";
 import useFetch from "../hooks/useFetch";
+import useClearableState from "../hooks/useClearableState";
 
 const ProductModal = ({ productId, closeHandler }) => {
   const [currentProduct, setCurrentProduct] = useState(null);
   const [isLoading, fetchProducts] = useFetch(`/api/products/${productId}`);
+  const [errorMessage, setErrorMessage, clearErrorMessage] = useClearableState();
 
   useEffect(() => {
     const successfulFetchHandler = ({ product }) => setCurrentProduct(product);
-    const failedFetchHandler = (err) => console.log("err", err);
+    const failedFetchHandler = ({ message }) => setErrorMessage(message);
 
     fetchProducts(null, successfulFetchHandler, failedFetchHandler);
   }, []);
@@ -26,6 +29,7 @@ const ProductModal = ({ productId, closeHandler }) => {
         {currentProduct && (
           <ProductDetailsView productDetails={currentProduct} />
         )}
+        {errorMessage && <ErrorModal message={errorMessage} closeHandler={clearErrorMessage} />}
       </Modal.Section>
     </Modal>
   );
